b23a71df113c27efe071f6ec959bf46fbca9ecb4
Asfarus1 <m.a.v.norm@mail.ru>
2023-12-02 12:04:01 +0400

    file compression

---
+++ compression/src/main.rs
@@ -0,0 +1,27 @@
+extern crate flate2;
+
+use flate2::write::GzEncoder;
+use flate2::Compression;
+use std::env::args;
+use std::fs::File;
+use std::io::{copy, BufReader};
+use std::time::Instant;
+
+fn main() {
+    if dbg!(args()).len() != 3 {
+        eprintln!("Usage: <input> <output>");
+        return;
+    }
+    let mut br = BufReader::new(File::open(args().nth(1).unwrap()).unwrap());
+    let output = File::create(args().nth(2).unwrap()).unwrap();
+    let mut encoder = GzEncoder::new(output, Compression::default());
+    let start = Instant::now();
+    copy(&mut br, &mut encoder).unwrap();
+    let output = encoder.finish().unwrap();
+    println!(
+        "Sourse len: {:?}\nCompressed len: {:?}\nTime: {:?} ms",
+        br.get_ref().metadata().unwrap().len(),
+        output.metadata().unwrap().len(),
+        start.elapsed()
+    );
+}
